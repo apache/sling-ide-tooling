458566968cb8cf6c1e7069118ab77e223478a4b0
Robert Munteanu <rombert@apache.org>
2018-04-19 18:53:33 +0300

    cli: assemble-app.sh now consults the remote repositories as well

--- cli/dist/assemble-app.sh
+++ cli/dist/assemble-app.sh
@@ -2,4 +2,4 @@
 
 rm -rf felix-cache launcher
 
-java -cp  ../../../whiteboard/featuremodel/feature-applicationbuilder/target/org.apache.sling.feature.applicationbuilder-0.0.1-SNAPSHOT.jar:${HOME}/.m2/repository/org/apache/felix/org.apache.felix.framework/5.6.8/org.apache.felix.framework-5.6.8.jar   org.apache.sling.feature.applicationbuilder.impl.Main   -d features/   -u file://${HOME}/.m2/repository   -o sling.json
+java -cp  ../../../whiteboard/featuremodel/feature-applicationbuilder/target/org.apache.sling.feature.applicationbuilder-0.0.1-SNAPSHOT.jar:${HOME}/.m2/repository/org/apache/felix/org.apache.felix.framework/5.6.8/org.apache.felix.framework-5.6.8.jar   org.apache.sling.feature.applicationbuilder.impl.Main   -d features/   -u file://${HOME}/.m2/repository,https://repo1.maven.org/maven2/   -o sling.json
