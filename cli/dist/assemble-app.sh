08343e460dcf336c3620ca509aad9751974e9ef0
Robert Munteanu <rombert@apache.org>
2018-09-08 20:22:09 +0200

    cli: adapt to latest feature model

--- cli/dist/assemble-app.sh
+++ cli/dist/assemble-app.sh
@@ -2,4 +2,15 @@
 
 rm -rf felix-cache launcher
 
-java -cp  ../../../whiteboard/featuremodel/feature-applicationbuilder/target/org.apache.sling.feature.applicationbuilder-0.0.1-SNAPSHOT.jar:${HOME}/.m2/repository/org/apache/felix/org.apache.felix.framework/5.6.8/org.apache.felix.framework-5.6.8.jar   org.apache.sling.feature.applicationbuilder.impl.Main   -d features/   -u file://${HOME}/.m2/repository,https://repo1.maven.org/maven2/   -o sling.json
+appbuilder_jar=bin/appbuilder.jar
+
+# download application builder, if needed
+if [ ! -f ${appbuilder_jar} ]; then
+    wget https://repository.apache.org/content/groups/snapshots/org/apache/sling/org.apache.sling.feature.applicationbuilder/0.1.0-SNAPSHOT/org.apache.sling.feature.applicationbuilder-0.1.0-20180906.104203-63.jar -O ${appbuilder_jar}
+fi
+
+# assemble application
+java -jar ${appbuilder_jar} \
+    -u file://$HOME/.m2/repository,https://repo.maven.apache.org/maven2,https://repository.apache.org/content/groups/snapshots \
+    -f features/clisync.json \
+    -o bin/app.json
--- cli/dist/run-app.sh
+++ cli/dist/run-app.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-rm -rf felix-cache launcher
+launcher_jar=bin/featurelauncher.jar
 
-java -jar ../../../whiteboard/featuremodel/feature-launcher/target/org.apache.sling.feature.launcher-0.0.1-SNAPSHOT.jar -a sling.json
+if [ ! -f ${launcher_jar} ]; then
+    wget https://repository.apache.org/content/groups/snapshots/org/apache/sling/org.apache.sling.feature.launcher/0.1.0-SNAPSHOT/org.apache.sling.feature.launcher-0.1.0-20180906.104202-54.jar -O ${launcher_jar}
+fi
+
+java -jar ${launcher_jar} \
+    -c $HOME/.m2/repository \
+    -u file://$HOME/.m2/repository,https://repo.maven.apache.org/maven2,https://repository.apache.org/content/groups/snapshots \
+    -f bin/app.json
