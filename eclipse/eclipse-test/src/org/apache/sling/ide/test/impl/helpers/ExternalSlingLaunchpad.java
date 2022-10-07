afab43a0fb11e8eefcfeca147d4a6738f0413283
Konrad Windszus <kwin@apache.org>
2022-10-07 16:37:02 +0200

    improve logging for failed server ready gates

--- eclipse/eclipse-test/src/org/apache/sling/ide/test/impl/helpers/ExternalSlingLaunchpad.java
+++ eclipse/eclipse-test/src/org/apache/sling/ide/test/impl/helpers/ExternalSlingLaunchpad.java
@@ -18,6 +18,7 @@ package org.apache.sling.ide.test.impl.helpers;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.Version;
 import java.net.http.HttpRequest;
@@ -31,11 +32,15 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.junit.rules.ExternalResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
 import junit.framework.AssertionFailedError;
@@ -65,21 +70,21 @@ public class ExternalSlingLaunchpad extends ExternalResource {
 
         long cutoff = System.currentTimeMillis() + MAX_WAIT_TIME_MS;
 
-        List<SlingReadyRule> rules = new ArrayList<>();
-        rules.add(new StartLevelSlingReadyRule(client, authorizationHeaderValue));
-        rules.add(new ActiveBundlesSlingReadyRule(client, authorizationHeaderValue));
-        rules.add(new RepositoryAvailableReadyRule(client, authorizationHeaderValue));
+        List<ServerReadyGate> gates = new ArrayList<>();
+        gates.add(new StartLevelGate(client, authorizationHeaderValue));
+        gates.add(new ActiveBundlesGate(client, authorizationHeaderValue));
+        gates.add(new RepositoryAvailableGate(client, authorizationHeaderValue));
         
         logger.debug("Starting check");
 
-        for (SlingReadyRule rule : rules) {
-            logger.debug("Checking {}", rule);
+        for (ServerReadyGate gate : gates) {
+            logger.debug("Checking {}", gate);
             while (true) {
-                if (rule.evaluate()) {
-                    logger.debug("Rule {} succeeded.", rule);
+                if (gate.evaluate()) {
+                    logger.debug("Gate {} passed.", gate);
                     break;
                 }
-                assertTimeout(cutoff, rule);
+                assertTimeout(cutoff, gate);
                 Thread.sleep(100);
             }
         }
@@ -87,24 +92,26 @@ public class ExternalSlingLaunchpad extends ExternalResource {
         logger.debug("Checks complete");
     }
 
-    private void assertTimeout(long cutoff, SlingReadyRule rule) throws AssertionFailedError {
-        logger.debug("Checking for timeout of rule {}, current {}, cutoff {}", rule, System.currentTimeMillis(), cutoff);
+    private void assertTimeout(long cutoff, ServerReadyGate gate) throws AssertionFailedError {
+        logger.debug("Checking for timeout of gate {}, current {}, cutoff {}", gate, System.currentTimeMillis(), cutoff);
         if (System.currentTimeMillis() > cutoff) {
-            throw new AssertionFailedError("Sling launchpad did not fulfill rule " + rule.getClass().getName() + " within " + MAX_WAIT_TIME_MS + " milliseconds" );
+            throw new AssertionFailedError("Sling server did not pass " + gate.getClass().getName() + " within " + MAX_WAIT_TIME_MS + " milliseconds. It was failing with " + gate.getFailureMessage() );
         }
     }
 
-    private interface SlingReadyRule {
+    private interface ServerReadyGate {
 
         boolean evaluate() throws Exception;
+        String getFailureMessage();
     }
 
-    private class StartLevelSlingReadyRule implements SlingReadyRule {
+    private class StartLevelGate implements ServerReadyGate {
 
         private final HttpClient client;
         private final HttpRequest request;
+        private int startLevel;
 
-        public StartLevelSlingReadyRule(HttpClient client, String authorizationHeaderValue) {
+        public StartLevelGate(HttpClient client, String authorizationHeaderValue) {
             this.client = client;
             request = HttpRequest.newBuilder()
             		.uri(config.getUrl().resolve("system/console/vmstat"))
@@ -120,10 +127,9 @@ public class ExternalSlingLaunchpad extends ExternalResource {
 
             if (response.statusCode() == 200) {
 
-
                 Matcher m = STARTLEVEL_JSON_SNIPPET.matcher(response.body());
                 if (m.find()) {
-                    int startLevel = Integer.parseInt(m.group(1));
+                    startLevel = Integer.parseInt(m.group(1));
                     logger.debug("vmstat http call got startLevel {}", startLevel);
                     if (startLevel >= EXPECTED_START_LEVEL) {
                         logger.debug("current startLevel {}  >= {}, we are done here", startLevel, EXPECTED_START_LEVEL);
@@ -134,13 +140,20 @@ public class ExternalSlingLaunchpad extends ExternalResource {
             }
             return false;
         }
+
+		@Override
+		public String getFailureMessage() {
+			return "Start Level " + startLevel + " was below expected level " + EXPECTED_START_LEVEL;
+		}
+        
     }
 
-    private class ActiveBundlesSlingReadyRule implements SlingReadyRule {
+    private class ActiveBundlesGate implements ServerReadyGate {
         private final HttpClient client;
         private final HttpRequest request;
+        private String failureMessage;
 
-        public ActiveBundlesSlingReadyRule(HttpClient client, String authorizationHeaderValue) {
+        public ActiveBundlesGate(HttpClient client, String authorizationHeaderValue) {
             this.client = client;
             request = HttpRequest.newBuilder()
             		.uri(config.getUrl().resolve("system/console/bundles.json"))
@@ -154,6 +167,7 @@ public class ExternalSlingLaunchpad extends ExternalResource {
             logger.debug("bundles http call got return code {}", response.statusCode());
             
             if (response.statusCode() != 200) {
+            	failureMessage = request.toString() + " returned " + response.statusCode();
                 return false;
             }
 
@@ -168,27 +182,66 @@ public class ExternalSlingLaunchpad extends ExternalResource {
                         int active = jsonReader.nextInt();
                         int fragment = jsonReader.nextInt();
                         logger.debug("bundle http call status: total = {}, active = {}, fragment = {}", total, active, fragment);
-
                         if (total == active + fragment) {
                             logger.debug("All bundles are started, we are done here");
                             return true;
-                        } else {
-                            return false;
                         }
+                    } else if (name.equals("data")) {
+                    	Type listType = new TypeToken<List<BundleMetadata>>() {}.getType();
+                    	List<BundleMetadata> bundles = new Gson().fromJson(jsonReader, listType);
+                    	failureMessage = "The following bundles were not started: " + bundles.stream()
+                    		.filter(b -> !(b.state == BundleMetadata.State.ACTIVE ||  b.state == BundleMetadata.State.RESOLVED))
+							.map(b -> b.symbolicName)
+							.collect(Collectors.joining(","));
                     } else {
-                        jsonReader.skipValue();
+                    	jsonReader.skipValue();
                     }
                 }
             }
             return false;
         }
+        
+
+		@Override
+		public String getFailureMessage() {
+			return failureMessage;
+		}
     }
-    
-    private class RepositoryAvailableReadyRule implements SlingReadyRule {
+
+    protected static final class BundleMetadata {
+		private String id;
+		private String name;
+		private String symbolicName;
+		@SerializedName("stateRaw")
+		private State state; // https://github.com/apache/felix-dev/blob/de64445723e33c400d4e851f2c2a874beb923119/webconsole/src/main/java/org/apache/felix/webconsole/internal/core/BundlesServlet.java#L438
+		
+		// https://docs.osgi.org/javadoc/osgi.core/8.0.0/org/osgi/framework/Bundle.html#getState--
+		// https://docs.osgi.org/javadoc/osgi.core/8.0.0/constant-values.html#org.osgi.framework.Bundle.ACTIVE
+		enum State {
+			@SerializedName("1")
+			UNINSTALLED,
+	
+			@SerializedName("2")
+			INSTALLED,
+	
+	        @SerializedName("4")
+			RESOLVED,
+	
+	        @SerializedName("8")
+			STARTING,
+	
+	        @SerializedName("16")
+			STOPPING,
+			@SerializedName("32")
+			ACTIVE;
+		}
+    }
+
+    private class RepositoryAvailableGate implements ServerReadyGate {
         private final HttpClient client;
         private final String authorizationHeaderValue;
 
-        public RepositoryAvailableReadyRule(HttpClient client, String authorizationHeaderValue) {
+        public RepositoryAvailableGate(HttpClient client, String authorizationHeaderValue) {
             this.client = client;
             this.authorizationHeaderValue = authorizationHeaderValue;
         }
@@ -196,7 +249,7 @@ public class ExternalSlingLaunchpad extends ExternalResource {
         @Override
         public boolean evaluate() throws Exception {
             
-            for ( String prefix: new String[] { "server", "crx/server"} ) {
+            for (String prefix: new String[] { "server", "crx/server"} ) {
             	HttpRequest request = HttpRequest.newBuilder()
                 		.uri(config.getUrl().resolve(prefix+"/default/jcr:root/content"))
                 		.header("Authorization", authorizationHeaderValue)
@@ -204,11 +257,16 @@ public class ExternalSlingLaunchpad extends ExternalResource {
                 
                 HttpResponse<Void> response = client.send(request, BodyHandlers.discarding());
                 logger.debug("repository check call at entry point {}  got status {}", prefix, response.statusCode());
-                if (response.statusCode() == 200 ) 
-                    return true;
+                if (response.statusCode() == 200 ) {
+                	return true;
+                }
             }
-            
             return false;
         }
+
+		@Override
+		public String getFailureMessage() {
+			return "WebDAV endpoint never returned a 200 status for repository root";
+		}
     }
 }
